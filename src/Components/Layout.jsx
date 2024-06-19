86bbe3488361b14362253d818f327c95885f5d0f
Nazianah <nazianah.umme@gmail.com>
2024-06-19 13:37:38 +0400

    Update Layout component styles and functionality

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,44 +2,72 @@ import React from 'react';
 import Button from './Button';
 import './Layout.css';
 
-const Layout = ({ layoutType, imageLink, buttonText, buttonLink, title, subtitle }) => {
-  return (
-    <div className={`layout ${layoutType}`}>
-      {layoutType === 'layout1' && (
-        <div className='layout1'>
-          <img src={imageLink} alt={title} className='layout-image' />
-          <div className='layout-content'>
-            <p>{subtitle}</p>
-            <h2>{title}</h2>
+
+const Layout = [
+  {
+    layoutType: 'layout1',
+    imageLink: 'https://www.rapp.com/media/jg2pyic5/home_showcase_about_dt_3200x728.jpg',
+    buttonText: 'About',
+    buttonLink: '/about',
+    title: 'We connect precision with empathy.',
+    subtitle: 'About'
+  },
+  {
+    layoutType: 'layout2',
+    imageLink: 'https://www.rapp.com/media/m4omjgkj/showcase-mastercard_desktop-1x.jpg',
+    buttonText: 'CREATING LINKS',
+    buttonLink: '/work',
+    title: 'How we made Mastercard Priceless for a wider audience',
+    subtitle: 'Work'
+  },
+  {
+    layoutType: 'layout3',
+    imageLink: 'https://www.rapp.com/media/tctiv2gj/showcase-image-3.jpg',
+    buttonText: 'CREATING LINKS',
+    buttonLink: '/locations',
+    title: 'We have offices all over the world',
+    subtitle: 'Locations'
+  }
+];
+
+// const Layout = ({ layoutType, imageLink, buttonText, buttonLink, title, subtitle }) => {
+//   return (
+//     <div className={`layout ${layoutType}`}>
+//       {layoutType === 'layout1' && (
+//         <div className='layout1'>
+//           <img src={imageLink} alt={title} className='layout-image' />
+//           <div className='layout-content'>
+//             <p>{subtitle}</p>
+//             <h2>{title}</h2>
             
-            <Button text={buttonText} link={buttonLink} className='primary' />
-          </div>
-        </div>
-      )}
-      {layoutType === 'layout2' && (
-        <div className='layout2'>
-          <div className='layout-content'>
-            <p>{subtitle}</p>
-            <h2>{title}</h2>
+//             <Button text={buttonText} link={buttonLink} className='primary' />
+//           </div>
+//         </div>
+//       )}
+//       {layoutType === 'layout2' && (
+//         <div className='layout2'>
+//           <div className='layout-content'>
+//             <p>{subtitle}</p>
+//             <h2>{title}</h2>
             
-            <Button text={buttonText} link={buttonLink} className='primary' />
-          </div>
-          <img src={imageLink} alt={title} className='layout-image' />
-        </div>
-      )}
-      {layoutType === 'layout3' && (
-        <div className='layout3'>
-          <div className='layout-content'>
-            <p>{subtitle}</p>
-            <h2>{title}</h2>
+//             <Button text={buttonText} link={buttonLink} className='primary' />
+//           </div>
+//           <img src={imageLink} alt={title} className='layout-image' />
+//         </div>
+//       )}
+//       {layoutType === 'layout3' && (
+//         <div className='layout3'>
+//           <div className='layout-content'>
+//             <p>{subtitle}</p>
+//             <h2>{title}</h2>
             
-            <Button text={buttonText} link={buttonLink} className='primary' />
-          </div>
-          <img src={imageLink} alt={title} className='layout-image' />
-        </div>
-      )}
-    </div>
-  );
-}
+//             <Button text={buttonText} link={buttonLink} className='primary' />
+//           </div>
+//           <img src={imageLink} alt={title} className='layout-image' />
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 export default Layout;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,12 +7,12 @@ const Home = () => {
   return (
     <>
     <div className='white'>
-    <BannerHomepage
-      imageLink={"https://www.rapp.com/media/k1qcbewy/home_banner5_dt_3200x1100.jpg"}
-      title={"We are RAPP Worldwide"}
-      subtitle={"A global precision marketing agency that knows how to speak to one individual, a million at a time."}
-    />
-    <Button text="FIND OUT HOW" link="/about" />
+      <BannerHomepage
+        imageLink={"https://www.rapp.com/media/k1qcbewy/home_banner5_dt_3200x1100.jpg"}
+        title={"We are RAPP Worldwide"}
+        subtitle={"A global precision marketing agency that knows how to speak to one individual, a million at a time."}
+      />
+      <Button text="FIND OUT HOW" link="/about" />
     </div>
     <Layout
         layoutType='layout1'
