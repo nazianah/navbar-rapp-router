43da44cfabf43270817896841daddc8ec91fdb7e
Nazianah <nazianah.umme@gmail.com>
2024-06-24 15:01:22 +0400

    Update Footer component styles in Footer.css and Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,24 +4,14 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
+            <h2>Ready to get in touch?</h2>
             <ul>
-                <li>
-                <h2>
-                Ready to get in touch?
-                </h2>
-                </li>
-                <li>
-                NEW BUSINESS INQUIRIES:
-                </li>
-                <li>
-                <a href="mailto:GlobalNewBusinessInquiries@rapp.com">GlobalNewBusinessInquiries@rapp.com</a>
-                </li>
-                <li>
-                PRESS INQUIRIES:
-                </li>
-                <li>
-                <a href="mailto:MediaInquiries@rapp.com">MediaInquiries@rapp.com</a>
-                </li>
+                <li>NEW BUSINESS INQUIRIES:</li>
+                <li><a href="mailto:GlobalNewBusinessInquiries@rapp.com">GlobalNewBusinessInquiries@rapp.com</a></li>
+            </ul>
+            <ul>
+                <li>PRESS INQUIRIES:</li>
+                <li><a href="mailto:MediaInquiries@rapp.com">MediaInquiries@rapp.com</a></li>
             </ul>
         </div>
     )
